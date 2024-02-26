ea34eb8d231aace4a749cdf20588bf4b43a10da3
tito300 <tarek.demachkie@gmail.com>
2024-02-25 18:58:55 -0600

    oauth wip

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,1 +1,7 @@
-console.log('background script loaded');
+const htmlFile = 'oauth.html';
+
+chrome.action.onClicked.addListener(() => {
+    console.log('#####')
+    chrome.tabs.create({ url: htmlFile });
+    chrome.action.setPopup({popup: 'src/pages/popup/index.html'})
+});
