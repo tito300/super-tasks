411c2e46dabc738150ec4585c205a2f15cd93f3d
Tarek Demachkie <tarek.demachkie@getslingshot.com>
2024-10-13 22:47:30 -0500

    add relative time

--- src/components/Task/components/TaskDate.tsx
+++ src/components/Task/components/TaskDate.tsx
@@ -5,20 +5,36 @@ import { useState } from "react";
 import { Button, Chip, Stack, Typography } from "@mui/material";
 import { CalendarMonth } from "@mui/icons-material";
 import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import isTomorrow from "dayjs/plugin/isTomorrow";
+import isToday from "dayjs/plugin/isToday";
+import isYesterday from "dayjs/plugin/isYesterday";
+
+dayjs.extend(isYesterday);
+dayjs.extend(isToday);
+dayjs.extend(isTomorrow);
+dayjs.extend(relativeTime);
 
 export function TaskDate({ onSubmit }: { onSubmit: () => void }) {
   const [focused, setFocused] = useState(false);
   const formFields = useFormContext<TaskForm>();
 
   const title = useWatch({ name: "title", control: formFields.control });
-  const due = formFields.getValues("due");
 
   return (
     <Controller
       control={formFields.control}
       name="due"
-      render={({ field }) =>
-        focused ? (
+      render={({ field }) => {
+        const label = dayjs(field.value).isToday()
+          ? "Today"
+          : dayjs(field.value).isTomorrow()
+          ? "Tomorrow"
+          : dayjs(field.value).isYesterday()
+          ? "Yesterday"
+          : dayjs(field.value).fromNow();
+
+        return focused ? (
           <DatePicker
             value={field.value ? dayjs(field.value) : null}
             onChange={(date) => {
@@ -30,12 +46,8 @@ export function TaskDate({ onSubmit }: { onSubmit: () => void }) {
             sx={{ py: 1, textTransform: "none" }}
             slotProps={{ textField: { size: "small" }, dialog: {} }}
           />
-        ) : due ? (
-          <Chip
-            onClick={() => setFocused(true)}
-            label={dayjs(due).format("DD/MM/YYYY")}
-            size="small"
-          />
+        ) : field.value ? (
+          <Chip onClick={() => setFocused(true)} label={label} size="small" />
         ) : (
           <Button
             disabled={!title}
@@ -48,8 +60,8 @@ export function TaskDate({ onSubmit }: { onSubmit: () => void }) {
               Date/time
             </Typography>
           </Button>
-        )
-      }
+        );
+      }}
     />
   );
 }
